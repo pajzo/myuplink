47c63b16fa7869a5f68696260202f5f07b72d25a
Peter Winkler <pwi@next.dk>
2022-12-09 23:09:53 +0100

    commit

--- myuplink/api.py
+++ myuplink/api.py
@@ -1,11 +1,4 @@
-from typing import Dict
+from .auth import AbstractAuth
 
 class MyUplinkApi:
-    def __init__(
-        self,
-        token: Dict[str, str] = None
-    ):
-        self.token = token
-
-    def get_token(self):
-        return self.token
+    
