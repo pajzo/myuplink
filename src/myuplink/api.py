11569b3547bd014bf7019ed692c336440dcbfcfe
Åke Strandberg <ake@strandberg.eu>
2024-02-05 17:19:26 +0100

    Add get json calls and enable language header

--- src/myuplink/api.py
+++ src/myuplink/api.py
@@ -13,21 +13,42 @@ class MyUplinkAPI:
 
     async def async_get_systems(self) -> List[System]:
         """Return systems."""
-        resp = await self.auth.request("get", "v2/systems/me")
-        resp.raise_for_status()
-        json = await resp.json()
+        json = await self.async_get_systems_json()
         array = json["systems"]        
         return [System(system_data) for system_data in array]
 
+    async def async_get_systems_json(self) -> dict:
+        """Return systems as json."""
+        resp = await self.auth.request("get", "v2/systems/me")
+        resp.raise_for_status()
+        return await resp.json()
+
     async def async_get_device(self, device_id) -> Device:
         """Return the device."""
+        json = await self.async_get_device_json(device_id)
+        return Device(json)
+
+    async def async_get_device_json(self, device_id) -> dict:
+        """Return the device as json."""
         resp = await self.auth.request("get", f"v2/devices/{device_id}")
         resp.raise_for_status()
-        return Device(await resp.json())
+        return await resp.json()
 
-    async def async_get_device_points(self, device_id) -> List[DevicePoint]:
+    async def async_get_device_points(self, device_id, language: str = "en-GB") -> List[DevicePoint]:
         """Return device points."""
-        resp = await self.auth.request("get", f"v2/devices/{device_id}/points")
+        array = await self.async_get_device_points_json(device_id, language)
+        return [DevicePoint(point_data) for point_data in array]
+
+    async def async_get_device_points_json(self, device_id, language: str = "en-GB") -> dict:
+        """Return device points as json."""
+        headers = {"Accept-Language": language}
+        resp = await self.auth.request("get", f"v2/devices/{device_id}/points", headers=headers)
+        resp.raise_for_status()
+        return await resp.json()
+
+    async def async_set_device_points(self, device_id, data: dict) -> dict:
+        """Return device points."""
+        resp = await self.auth.request("patch", f"v2/devices/{device_id}/points", json=data)
         resp.raise_for_status()
         array = await resp.json()
-        return [DevicePoint(point_data) for point_data in array]
+        return array
