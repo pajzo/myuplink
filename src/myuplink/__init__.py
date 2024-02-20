30c11d2150eac8918de39819baf6b6a82b720b4b
Åke Strandberg <ake@strandberg.eu>
2024-02-20 18:55:00 +0000

    Remove more exceptions

--- src/myuplink/__init__.py
+++ src/myuplink/__init__.py
@@ -2,9 +2,4 @@
 from .api import MyUplinkAPI  # noqa: F401
 from .auth import Auth  # noqa: F401
 from .auth_abstract import AbstractAuth  # noqa: F401
-from .exceptions import (  # noqa:F401
-    MyUplinkException,
-    MyUplinkAuthException,
-    MyUplinkConflictException,
-)
 from .models import System, SystemDevice, Device, DevicePoint, EnumValue  # noqa: F401
